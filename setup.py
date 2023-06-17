463361245e618b0db5bab7c21a6f708274f818ed
Youssef Hussein <youssef.m.hussein@gmail.com>
2023-06-17 18:22:23 +0300

    Minor bug fixes and enhancement

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='bump-helm-chart-version',
+    name='bump_helm_chart_version',
     version='1.0.0',
     description='Bump helm chart version if the /templates folder has changes',
     author='Youssef Hussein',
     author_email='youssef@doubl.tech',
     packages=['bump_helm_chart_version'],
-    install_requires=['pyyaml'],
     entry_points={
         'console_scripts': [
             'bump-chart-version = bump_helm_chart_version.bump_chart_version:main',
