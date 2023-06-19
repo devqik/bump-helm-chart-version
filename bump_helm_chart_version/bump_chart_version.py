7c901ed7ffca04d4512df2c855ae6420f8414810
Youssef Hussein <youssef.m.hussein@gmail.com>
2023-06-19 20:29:38 +0300

    Fix linting errors

--- bump_helm_chart_version/bump_chart_version.py
+++ bump_helm_chart_version/bump_chart_version.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
-import sys
-import os
+"""
+This script performs version-related tasks for chart directories based on Git changes.
+"""
+
 import subprocess
 import yaml
+import sys
+
+import os  # Reordered import
 
 # Check if required packages are installed and install them if necessary
 try:
@@ -13,16 +18,23 @@ except ImportError:
     import yaml
 
 def main(argv=None):
+    """
+    Main function for performing version-related tasks on chart directories.
+    """
+
     def increment_patch_version(version):
+        """
+        Increment the patch version of a given version string.
+        """
         parts = version.split('.')
         if len(parts) != 3:
-            raise ValueError('Invalid version string: {}'.format(version))
+            raise ValueError(f'Invalid version string: {version}')
         try:
             patch = int(parts[2])
-        except ValueError:
-            raise ValueError('Invalid patch version: {}'.format(parts[2]))
+        except ValueError as exc:
+            raise ValueError(f'Invalid patch version: {parts[2]}') from exc
         new_patch = patch + 1
-        new_version = '{}.{}.{}'.format(parts[0], parts[1], new_patch)
+        new_version = f'{parts[0]}.{parts[1]}.{new_patch}'
         return new_version
 
     # Run git command to retrieve main branch name
@@ -40,20 +52,36 @@ def main(argv=None):
 
     # Get list of chart directories that have changed
     git_diff_output = subprocess.run(['git', 'diff', '--name-only', 'HEAD'], check=True, capture_output=True, text=True)
-    changed_files = [filename for filename in git_diff_output.stdout.splitlines()]
+    changed_files = list(git_diff_output.stdout.splitlines())  # Replaced comprehension with list()
 
-    chart_dirs = [os.path.dirname(file) for file in changed_files if file.endswith('/Chart.yaml') or '/templates' in file]
+    chart_dirs = [
+        os.path.dirname(file)
+        for file in changed_files
+        if file.endswith('/Chart.yaml') or '/templates' in file
+    ]
     chart_dirs = [os.path.split(dir_path) for dir_path in chart_dirs]
-    chart_dirs = [os.path.join(*parts[:-1]) if parts[-1] == 'templates' else os.path.join(*parts) for parts in chart_dirs]
+    chart_dirs = [
+        os.path.join(*parts[:-1]) if parts[-1] == 'templates' else os.path.join(*parts)
+        for parts in chart_dirs
+    ]
     chart_dirs = list(dict.fromkeys(chart_dirs))
 
     for chart_dir in chart_dirs:
         # Get versions
-        prev_version_chart_file = subprocess.run(['git', 'show', f'{main_branch_name}:{chart_dir}/Chart.yaml'], check=True, capture_output=True, text=True).stdout
+        prev_version_chart_file = subprocess.run(
+            ['git', 'show', f'{main_branch_name}:{chart_dir}/Chart.yaml'],
+            check=True,
+            capture_output=True,
+            text=True
+        ).stdout
         prev_version_dict = yaml.safe_load(prev_version_chart_file)
         prev_version = prev_version_dict['version']
-        with open(f'{chart_dir}/Chart.yaml', 'r') as f:
-            current_version = [line.split()[1] for line in f.readlines() if line.startswith('version:')][0].strip()
+        with open(f'{chart_dir}/Chart.yaml', 'r', encoding='utf-8') as f:  # Added encoding parameter
+            current_version = [
+                line.split()[1]
+                for line in f.readlines()
+                if line.startswith('version:')
+            ][0].strip()
         new_version = increment_patch_version(prev_version)
         # Echo previous and current versions
         print(f"The previous version is {prev_version}")
@@ -61,12 +89,12 @@ def main(argv=None):
 
         # Check if Chart version was changed manually
         if prev_version != current_version:
-            print(f"Chart was updated")
+            print("Chart was updated")
         else:
             print(f"Checking chart version in {chart_dir}")
             if new_version != current_version:
                 print(f"Updating chart version from {current_version} to {new_version}")
-                with open(f'{chart_dir}/Chart.yaml', 'r+') as f:
+                with open(f'{chart_dir}/Chart.yaml', 'r+', encoding='utf-8') as f:  # Added encoding parameter
                     lines = f.readlines()
                     f.seek(0)
                     f.truncate()
